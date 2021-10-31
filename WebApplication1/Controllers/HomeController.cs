504ca3a3a92ba4f3fc427fcce40a45f01b9d075c
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2021-10-31 16:05:54 +0300

    first commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            HomeViewModel hm = new();
+            // Start the child process.
+            Process p = new Process();
+            // Redirect the output stream of the child process.
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.FileName = "C:\\Debug\\ConsoleApp4.exe";
+            p.Start();
+            var output=p.StandardOutput.ReadToEnd();
+            /*
+            var output = new List<string>();
+
+            while (p.StandardOutput.Peek() > -1)
+            {
+                output.Add(p.StandardOutput.ReadLine());
+            }
+
+            while (p.StandardError.Peek() > -1)
+            {
+                output.Add(p.StandardError.ReadLine());
+            }*/
+            hm.Mesaj = output;
+            p.WaitForExit();
+            return View(hm);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
